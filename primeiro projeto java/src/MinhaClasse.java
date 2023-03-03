a51daff51ff8cc16ba3d1a3b6cb51d7b250be391
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-03-03 15:53:43 -0300

    Exemplo NomeCompleto (variavel e metodo)

--- primeiro projeto java/src/MinhaClasse.java
+++ primeiro projeto java/src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
-    public static void main(String[] args){
-        System.out.println("Boas vindas turma DIO");
+    public static void main(String[] args) {
+    System.out.println("Boas vindas turma DIO");
+    
+    String PrimeiroNome = "Gabriel";
+    String SegundoNome = "Iope";
+    
+    String nomeCompleto = nomeCompleto(PrimeiroNome, SegundoNome);
+    System.out.println(nomeCompleto);
     }
+    
+    public static String nomeCompleto (String PrimeiroNome, String SegundoNome){
+        return "Resultado do método: " + PrimeiroNome.concat(" ").concat(SegundoNome);
+    }
+
 }
