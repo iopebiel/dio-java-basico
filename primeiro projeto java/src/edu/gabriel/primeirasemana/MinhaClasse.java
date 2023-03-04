e150ce4c850bc35cd3d0f1585a843cab107caaa2
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-03-03 23:08:21 -0300

    Sepraração em pacote

--- primeiro projeto java/src/edu/gabriel/primeirasemana/MinhaClasse.java
+++ primeiro projeto java/src/edu/gabriel/primeirasemana/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.gabriel.primeirasemana;
+
 public class MinhaClasse {
     public static void main(String[] args) {
     System.out.println("Boas vindas turma DIO");
--- primeiro projeto java/src/edu/gabriel/segundasemana/BoletimEstudantil.java
+++ primeiro projeto java/src/edu/gabriel/segundasemana/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.gabriel.segundasemana;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 7;
