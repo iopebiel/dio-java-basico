fddbcc1860ad1d9ad783b6fa579a0b884f1f2ed8
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-03-04 20:28:33 -0300

    Tipos de Variaveis

---
+++ primeiro projeto java/src/edu/gabriel/segundasemana/TipoVariavel.java
@@ -0,0 +1,21 @@
+package edu.gabriel.segundasemana;
+
+public class TipoVariavel {
+
+    public static void main(String[] args) {
+        //String meuNome = "Gabriel Iope";
+
+        //double salarioMinimo = 2500.34;
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal; //não recomendado, precisão maior para uma menor 
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //final double VALOR_DE_PI = 3.14;
+    }
+}
