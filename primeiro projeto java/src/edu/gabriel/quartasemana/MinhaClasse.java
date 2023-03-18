f539a637ec11a5a997c17ae2282f8133b764a5dd
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-03-18 18:51:29 -0300

    Execução da IDE

---
+++ primeiro projeto java/src/edu/gabriel/quartasemana/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.gabriel.quartasemana;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, fui executado diretamente da IDE!");
+    }
+}
