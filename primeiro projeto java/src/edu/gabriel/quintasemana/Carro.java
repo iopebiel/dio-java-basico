5321e1f1d43f3c68925930e06c38098c5cded6a7
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-04-09 23:19:39 -0300

    POO atributos

--- primeiro projeto java/src/edu/gabriel/quintasemana/Carro.java
+++ primeiro projeto java/src/edu/gabriel/quintasemana/Carro.java
@@ -1,5 +1,7 @@
 package edu.gabriel.quintasemana;
 
 public class Carro { //Classe carro
-
+    String cor; //atributos e seu tipo de dado que ele representa.
+    String modelo;
+    int capacidadeTanque;
 }
