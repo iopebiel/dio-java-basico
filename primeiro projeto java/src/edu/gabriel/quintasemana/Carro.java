b8b3540c2bcb639ac6622f627aed874d387e8fc8
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-04-10 00:00:10 -0300

    POO Método

--- primeiro projeto java/src/edu/gabriel/quintasemana/Carro.java
+++ primeiro projeto java/src/edu/gabriel/quintasemana/Carro.java
@@ -4,4 +4,50 @@ public class Carro { //Classe carro
     String cor; //atributos e seu tipo de dado que ele representa.
     String modelo;
     int capacidadeTanque;
+//metodo construtor, com o objeto carro vazio, sem informações
+    Carro(){
+
+    }
+
+
+//metodo construtor, com valores iniciais a partir desse método. Uso de sobrecarga. Mesmo nome e parametros diferentes.
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+//metodos set e get
+
+    void setcor(String cor) //setar/colocar um valor no atributo cor
+    {
+        this.cor = cor;
+    }
+
+    String getCor(){ //retornar o valor da cor.
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getcapacidadeTanque(){
+        return capacidadeTanque;
+    }
+
+//metodos de contrato
+
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
 }
